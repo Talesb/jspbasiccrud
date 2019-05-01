bd3dace1d32381d7c8a454b73f5bf1f085d2b458
Talesb <talesbatistafig@hotmail.com>
2019-05-01 00:20:04 -0300

    Adicionando js e css

---
+++ src/main/java/br/com/unesa/crudcliente/modelo/Cliente.java
@@ -0,0 +1,42 @@
+package br.com.unesa.crudcliente.modelo;
+
+public class Cliente {
+
+	private int id;
+	private String nome;
+	private String sobrenome;
+	private double gastos;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public double getGastos() {
+		return gastos;
+	}
+
+	public void setGastos(double gastos) {
+		this.gastos = gastos;
+	}
+
+}
